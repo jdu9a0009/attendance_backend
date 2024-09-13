9223058871a39cbe11478cd85d50ad073638b944
jdu9a0009 <t19a0009s@jdu.uz>
2024-09-13 16:35:30 +0500

    update

--- internal/controller/http/v1/attendance/attendance.go
+++ internal/controller/http/v1/attendance/attendance.go
@@ -19,43 +19,11 @@ type Controller struct {
 func NewController(attendance Attendance) *Controller {
 	return &Controller{attendance}
 }
-
-type OfficeLocation struct {
-	Latitude  float64
-	Longitude float64
-	Radius    float64 // in meters
-}
-
-var OfficeLocations = []OfficeLocation{
-	{
-		Latitude:  41.3330625,
-		Longitude: 69.2550781,
-		Radius:    2000.0,
-	},
-	{
-		Latitude:  41.3171712,
-		Longitude: 69.3108736,
-		Radius:    2000.0,
-	},
-	{
-		Latitude:  41.319006,
-		Longitude: 69.303411,
-		Radius:    3000.0,
-	},
-	{
-		Latitude:  41.3335552,
-		Longitude: 69.3108736,
-		Radius:    3000.0,
-	},
-	{
-		Latitude:  35.7031509,
-		Longitude: 139.7745439,
-		Radius:    3000.0,
-	},
-}
-
-//Attendance
-
+const (
+	OfficeLatitude  = 41.3330625
+	OfficeLongitude = 69.2550781
+	OfficeRadius    = 3000 // in meters
+)
 func (uc Controller) GetList(c *web.Context) error {
 	var filter attendance.Filter
 
@@ -277,74 +245,63 @@ func (uc Controller) Delete(c *web.Context) error {
 	}, http.StatusOK)
 }
 
-	func (uc Controller) CreateByPhone(c *web.Context) error {
-		var request attendance.EnterRequest
-		if err := c.BindFunc(&request, "Latitude,Longitude"); err != nil {
-			return c.RespondError(err)
-		}
-	
-		// Check distance to each office
-		for _, office := range OfficeLocations {
-			distance := CalculateDistance(request.Latitude, request.Longitude, office.Latitude, office.Longitude)
-			if distance <= office.Radius {
-				response,  err := uc.attendance.CreateByPhone(c.Ctx, request)
-				if err != nil {
-					return c.RespondError(err)
-				}
-	
-				return c.Respond(map[string]interface{}{
-					"data":    response,
-					"status":  true,
-				}, http.StatusOK)
-			}
-		}
-	
-		return c.RespondError(web.NewRequestError(errors.New("distance from all offices is greater than office radius"), http.StatusBadRequest))
-	}
 func (uc Controller) CreateByQRCode(c *web.Context) error {
 	var request attendance.EnterRequest
 	if err := c.BindFunc(&request, "Latitude,Longitude"); err != nil {
 		return c.RespondError(err)
 	}
+    distance := CalculateDistance(request.Latitude, request.Longitude, OfficeLatitude, OfficeLongitude)
+    if distance <= OfficeRadius{
+      response, message, err := uc.attendance.CreateByQRCode(c.Ctx, request)
+      if err != nil {
+        return c.RespondError(err)
+      }
 
-	// Check distance to each office
-	for _, office := range OfficeLocations {
-		distance := CalculateDistance(request.Latitude, request.Longitude, office.Latitude, office.Longitude)
-		if distance <= office.Radius {
-			response, message, err := uc.attendance.CreateByQRCode(c.Ctx, request)
-			if err != nil {
-				return c.RespondError(err)
-			}
+      return c.Respond(map[string]interface{}{
+        "data":    response,
+        "message": message,
+        "status":  true,
+      }, http.StatusOK)
+    }
+	return c.RespondError(web.NewRequestError(errors.New("distance from office is greater than office radius"), http.StatusBadRequest))
 
-			return c.Respond(map[string]interface{}{
-				"data":    response,
-				"message": message,
-				"status":  true,
-			}, http.StatusOK)
-		}
+}
+func (uc Controller) CreateByPhone(c *web.Context) error {
+	var request attendance.EnterRequest
+	if err := c.BindFunc(&request, "Latitude,Longitude"); err != nil {
+		return c.RespondError(err)
 	}
-
-	return c.RespondError(web.NewRequestError(errors.New("distance from all offices is greater than office radius"), http.StatusBadRequest))
+	distance := CalculateDistance(request.Latitude, request.Longitude, OfficeLatitude, OfficeLongitude)
+	if distance <= OfficeRadius {
+		response, err := uc.attendance.CreateByPhone(c.Ctx, request)
+		if err != nil {
+			return c.RespondError(err)
+		}
+		return c.Respond(map[string]interface{}{
+			"data":   response,
+			"status": true,
+		}, http.StatusOK)
+	}
+	return c.RespondError(web.NewRequestError(errors.New("distance from office is greater than office radius"), http.StatusBadRequest))
 }
 func (uc Controller) ExitByPhone(c *web.Context) error {
 	var request attendance.EnterRequest
 	if err := c.BindFunc(&request, "Latitude,Longitude"); err != nil {
 		return c.RespondError(err)
 	}
-
-	response, err := uc.attendance.ExitByPhone(c.Ctx, request)
-	if err != nil {
-		return c.RespondError(err)
+	distance := CalculateDistance(request.Latitude, request.Longitude, OfficeLatitude, OfficeLongitude)
+	if distance <= OfficeRadius {
+		response, err := uc.attendance.ExitByPhone(c.Ctx, request)
+		if err != nil {
+			return c.RespondError(err)
+		}
+		return c.Respond(map[string]interface{}{
+			"data":   response,
+			"status": true,
+		}, http.StatusOK)
 	}
-
-	return c.Respond(map[string]interface{}{
-		"data":      response,
-		"latitude":  request.Latitude,
-		"longitude": request.Longitude,
-		"status":    true,
-	}, http.StatusOK)
+	return c.RespondError(web.NewRequestError(errors.New("distance from office is greater than office radius"), http.StatusBadRequest))
 }
-
 func CalculateDistance(lat1, lon1, lat2, lon2 float64) float64 {
 	// Haversine formula to calculate the great-circle distance between two points
 	R := 6371.0 // Earth's radius in kilometers
@@ -352,10 +309,8 @@ func CalculateDistance(lat1, lon1, lat2, lon2 float64) float64 {
 	φ2 := lat2 * math.Pi / 180.0
 	Δφ := (lat2 - lat1) * math.Pi / 180.0
 	Δλ := (lon2 - lon1) * math.Pi / 180.0
-
 	a := math.Sin(Δφ/2)*math.Sin(Δφ/2) + math.Cos(φ1)*math.Cos(φ2)*math.Sin(Δλ/2)*math.Sin(Δλ/2)
 	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
 	distance := R * c * 1000 // Distance in meters
-
 	return distance
 }
