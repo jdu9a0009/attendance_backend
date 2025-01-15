bb79f3818cb8c5033c5d20c996c1b3cfb14fa5b9
khdiyz <khdiyz.12@gmail.com>
2025-01-15 14:40:18 +0500

    fix cors

--- internal/middleware/cors_middleware.go
+++ internal/middleware/cors_middleware.go
@@ -2,18 +2,32 @@ package middleware
 
 import "github.com/gin-gonic/gin"
 
-func CorsMiddleware() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		ctx.Writer.Header().Set("Content-Type", "application/json")
-		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With,Access-Control-Request-Method, Access-Control-Request-Headers")
-		ctx.Header("Access-Control-Max-Age", "3600")
-		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH, HEAD")
-		if ctx.Request.Method == "OPTIONS" {
-			ctx.AbortWithStatus(204)
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		origin := c.Request.Header.Get("Origin")
+		
+		// Ruxsat berilgan domenlar ro'yxati
+		allowedOrigins := map[string]bool{
+			"https://attendance.eduflow.uz": true,
+			"http://localhost:3000":        true,
+		}
+
+		// Ruxsat berilgan domenni tekshirish
+		if _, ok := allowedOrigins[origin]; ok {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
+		c.Writer.Header().Set("Vary", "Origin") // Brauzer kechasi uchun muhim
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, DELETE, GET, PUT, OPTIONS")
+
+		// OPTIONS so‘rovlarini qayta ishlash
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
 			return
 		}
-		ctx.Next()
+
+		c.Next()
 	}
 }
