b1d0edbae6d117359c1dd2aff5c92656999dc20a
Diyorbek Hasanov <khdiyz@adsl-172-15-159-64.lightspeed.ltrkar.sbcglobal.net>
2025-01-15 13:53:30 +0500

    fix cors

--- internal/middleware/cors_middleware.go
+++ internal/middleware/cors_middleware.go
@@ -8,7 +8,6 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 		c.Writer.Header().Set("Vary", "Origin")
 
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, DELETE, GET, PUT")
 
